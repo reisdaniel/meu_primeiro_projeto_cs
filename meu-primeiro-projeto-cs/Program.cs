ed02d00c07cb7e7bcab3e850918b0789f236f669
DanielReis <drm1498@gmail.com>
2023-03-22 15:59:36 -0300

    Melhorias no código para torná-lo mais robusto e fácil de usar.

--- meu-primeiro-projeto-cs/Program.cs
+++ meu-primeiro-projeto-cs/Program.cs
@@ -1,18 +1,45 @@
 ﻿using System;
 
-namespace meu_primeiro_projeto_cs
+namespace Calculadora
 {
-    class Operacoes_matematicas
+    class Program
     {
         static void Main(string[] args) 
         {
-            while (true)
-            {
-                Console.WriteLine("Digite o primeiro número: ");
-                double num1 = double.Parse(Console.ReadLine());
+            bool sair = false;
 
-                Console.WriteLine("Digite o segundo número: ");
-                double num2 = double.Parse(Console.ReadLine());
+            while (!sair)
+            {
+                double num1 = 0, num2 = 0;
+                bool numValido = false;
+
+                while (!numValido)
+                {
+                    Console.WriteLine("Digite o primeiro número: ");
+                    if (!double.TryParse(Console.ReadLine(), out num1))
+                    {
+                        Console.WriteLine("Número inválido. Por favor, tente novamente.");
+                    }
+                    else
+                    {
+                        numValido = true;
+                    }
+                }
+
+                numValido = false;
+
+                while (!numValido)
+                {
+                    Console.WriteLine("Digite o segundo número: ");
+                    if (!double.TryParse(Console.ReadLine(), out num2))
+                    {
+                        Console.WriteLine("Número inválido. Por favor, tente novamente");
+                    }
+                    else
+                    {
+                        numValido = true;
+                    }
+                }
 
                 Console.WriteLine("Escolha uma operação matemática:");
                 Console.WriteLine("1. Soma");
@@ -20,7 +47,21 @@ namespace meu_primeiro_projeto_cs
                 Console.WriteLine("3. Multiplicação");
                 Console.WriteLine("4. Divisão");
 
-                int escolha = int.Parse(Console.ReadLine());
+                int escolha = 0;
+                bool escolhaValida = false;
+
+                while (!escolhaValida)
+                {
+                    Console.WriteLine("Opção escolhida: ");
+                    if (!int.TryParse(Console.ReadLine(), out escolha) || escolha < 1 || escolha > 4)
+                    {
+                        Console.WriteLine("Opção inválida. Por favor, escolha um número de 1 a 4.");
+                    }
+                    else
+                    {
+                        escolhaValida = true;
+                    }
+                }
 
                 double resultado = 0;
 
@@ -36,21 +77,46 @@ namespace meu_primeiro_projeto_cs
                         resultado = num1 * num2;
                         break;
                     case 4:
-                        resultado = num1 / num2;
-                        break;
-                    default:
-                        Console.WriteLine("Opção inválida");
+                        if (num2 == 0)
+                        {
+                            Console.WriteLine("Não é possível dividir por zero. Por favor, tente novamente.");
+                        }
+                        else
+                        {
+                            resultado = num1 / num2;
+                        }
                         break;
                 }
 
-                Console.WriteLine("Resultado: " +  resultado);
-
-                Console.WriteLine("Deseja realizar outra operação? (S/N)");
-                string opcao = Console.ReadLine();
-
-                if (opcao.ToUpper() == "N")
+                if (escolha == 4 && num2 == 0) 
                 {
-                    break;
+                    //não faz nada
+                }
+                else
+                {
+                    Console.WriteLine("Resultado: " + resultado);
+                }
+
+                bool opcaoValida = false;
+
+                while (!opcaoValida) 
+                {
+                    Console.WriteLine("Deseja realizar outra operação? (S/N): ");
+                    string opcao = Console.ReadLine().ToUpper();
+
+                    if (opcao == "N")
+                    {
+                        sair = true;
+                        opcaoValida = true;
+                    }
+                    else if (opcao == "S") 
+                    {
+                        opcaoValida = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Opção inválida. Por favor, digite S ou N.");
+                    }
                 }
             }
         }
